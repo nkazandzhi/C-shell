026722ff6ca602c65bacfe1a37a0284fe14ff437
Nikol Kazandzhieva <75629346+nkazandzhi@users.noreply.github.com>
2024-05-10 18:17:42 +0300

    Update 2016-SE-02.sh

--- 2016-SE-02.sh
+++ 2016-SE-02.sh
@@ -10,3 +10,22 @@ if ! egrep -q '^[+-]?[0-9]+$' <(echo "${1}"); then
   exit 2
 fi
 
+USERS=$(mktemp)
+ps -eo user= | sort | uniq > "${USERS}"
+
+while read USER; do
+  USER_TOTAL_RSS=0
+  while read PID RSS; do
+    USER_TOTAL_RSS=$(("${USER_TOTAL_RSS}" + "${RSS}"))
+    LAST_PID="${PID}"
+  done < <(ps -u "${USER}" -o pid=,rss= | sort -n -k2)
+
+  echo "Total "${USER_TOTAL_RSS}" RRS for user "${USER}"."
+
+  if [ "${USER_TOTAL_RSS}" -gt "${1}" ]; then
+  #kill -s TERM "${LAST_PID}"
+  echo "Kill a process with PIID "${LAST_PID}" for user "${USER}"."
+  fi
+done < "${USERS}"
+
+rm "${USERS}"
