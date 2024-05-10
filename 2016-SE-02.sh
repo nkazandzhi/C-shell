225e695e0dd51e99d7cd8df4a709c05676632fea
Nikol Kazandzhieva <75629346+nkazandzhi@users.noreply.github.com>
2024-05-10 18:08:53 +0300

    Update 2016-SE-02.sh

--- 2016-SE-02.sh
+++ 2016-SE-02.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+  echo "Wrong number of integers."
+  exit 1
+fi
+
+if ! egrep -q '^[+-]?[0-9]+$' <(echo "${1}"); then
+  echo "Invalid argument"
+  exit 2
+fi
+
