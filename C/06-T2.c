a36bf8e96e5a4e8336e24033d12bf4daa5422788
Nikol Kazandzhieva <75629346+nkazandzhi@users.noreply.github.com>
2024-08-24 14:47:20 +0300

    Create 06-T2.c
    Реализирайте команда head без опции (т.е. винаги да извежда на стандартния изход само първите 10 реда от съдържанието на файл подаден като първи параматър)

---
+++ C/06-T2.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[])
+{
+	int fd_file; 
+	if ((fd_file = open(argv[1], O_RDONLY)) == -1 ){
+		write(1, "Cant open", 9);
+		exit (-1);
+	}
+
+	int line = 0;
+	char c;
+
+	//while (read(fd_file, &c, sizeof(c)) == sizeof(c)) { 
+	while (read(fd_file, &c,1))
+	{
+		if (c == '\n')
+		{
+			line = line+1;
+		}
+		write(1,&c,1);
+
+		if(line == 10){
+			break;
+		}
+	}
+  close(fd_file);
+}
