9f7e88fa54ae24e973f7d2b598ed2578e9f5f55f
Nikol Kazandzhieva <75629346+nkazandzhi@users.noreply.github.com>
2024-08-28 18:09:45 +0300

    Create 2017-SE-04.c

---
+++ C/2017-SE-04.c
@@ -0,0 +1,141 @@
+//сестри са с 2017-SE-02
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+ #include <fcntl.h>
+
+int main(int argc, char* argv[])
+{
+
+
+		//we dont count lines
+		//just read row by row
+		for(int i = 1; i < argc;i++)
+		{
+			if(strcmp(argv[i],"-") == 0)
+			{
+				
+				ssize_t readb;
+				char c;
+				while(readb = read(0, &c,sizeof(c)) >0)
+				{
+					
+					if (write(1,&c,sizeof(c)) == -1)
+					{
+						err(1,"Error");
+					}
+				
+				}
+			}
+			else
+			{
+
+				
+				ssize_t readb;
+				char c;
+				int fd = open(argv[i],O_RDONLY);
+				if (fd == -1) {
+					err(3, "Error: cant open");
+				}
+				while(readb = read(fd, &c,sizeof(c)) >0)
+				{
+					
+					if (write(1,&c,sizeof(c)) == -1)
+					{
+						err(1,"Error");
+					}
+				
+				}
+				close(fd);
+			}
+		}
+
+	exit(0);	
+}
+
+
+//v2
+#include <stdlib.h>
+#include <unistd.h>
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+
+void close_safe(int fd);
+void write_safe(int fd, char c, int size);
+int open_safe(const char* file_name);
+void read_safe(int fd, const char* file_name);
+
+void close_safe(int fd) {
+    int errno_ = errno;
+    if (fd >= 0) {
+        close(fd);
+    }
+
+    errno = errno_;
+}
+
+void write_safe(int fd, char c, int size) {
+    if (write(1, &c, size) != size) {
+        close_safe(fd);
+        err(1, "Could not write to stdout");
+    }
+}
+
+int open_safe(const char* file_name) {
+    int fd;
+    if ((fd = open(file_name, O_RDONLY)) == -1) {
+        err(3, "Could not open file %s", file_name);
+    }
+
+    return fd;
+}
+
+void read_safe(int fd, const char* file_name) {
+    char c;
+    int bytes_read;
+    while((bytes_read = read(fd, &c, sizeof(c))) == sizeof(c)) {
+        write_safe(fd, c, sizeof(c));
+    }
+
+    if (bytes_read == -1) {
+        close_safe(fd);
+        err(2, "Could not read from %s", file_name);
+    }
+
+    write_safe(fd, '\n', sizeof(char));
+    close(fd);
+}
+
+int main(int argc, char** argv) {
+    if (argc == 1) {
+        read_safe(0, "stdin");
+        exit(0);
+    }
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-") == 0) {
+            read_safe(0, "stdin");
+        } else {
+            int fd = open_safe(argv[i]);
+            read_safe(fd, argv[i]);
+        }
+    }
+
+    exit(0);
+}
+
+//Напишете програма на C, която да работи подобно на командата cat, реализирайки
+//само следната функционалност:
+//• програмата извежда на STDOUT
+//• ако няма подадени параметри, програмата чете от STDIN
+//• ако има подадени параметри – файлове, програмата последователно ги извежда
+//• ако някой от параметрите започва с тире (-), програмата да го третира като специално име
+//        за STDIN
+//        Примерно извикване:
+//$ ./main f - g
+//– извежда съдържанието на файла f, после STDIN, след това съдържанието на файла g.
