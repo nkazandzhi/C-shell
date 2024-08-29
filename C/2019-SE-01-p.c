629e02cb09e61e0c4c98175a2522f02f3f1908c7
Nikol Kazandzhieva <75629346+nkazandzhi@users.noreply.github.com>
2024-08-29 21:36:46 +0300

    Create 2019-SE-01-p.c
    //Напишете програма-наблюдател P, която изпълнява друга програма Q и я рестартира, когато Q завърши изпълнението си. На командния ред на P се подават следните параметри:
    //• праг за продължителност в секунди – едноцифрено число от 1 до 9
    //• Q
    //• незадължителни параметри на Q
    //P работи по следния алгоритъм:
    //• стартира Q с подадените параметри
    //• изчаква я да завърши изпълнението си
    //• записва в текстов файл run.log един ред с три полета - цели числа (разделени с интервал):
    //– момент на стартиране на Q (Unix time)
    //– момент на завършване на Q (Unix time)
    //– код за грешка, с който Q е завършила (exit code)
    //• проверява дали е изпълнено условието за спиране и ако не е, преминава отново към стартирането на Q
    //Условие за спиране: Ако наблюдателят P установи, че при две последователни изпълнения на Q са
    //        били изпълнени и двете условия:
    //1. кодът за грешка на Q е бил различен от 0;
    //2. разликата между момента на завършване и момента на стартиране на Q е била по-малка от
    //подадения като първи параметър на P праг;
    //то P спира цикъла от изпълняване на Q и сам завършва изпълнението си.
    //Текущото време във формат Unix time (секунди от 1 януари 1970 г.) можете да вземете с извикване
    //на системната функция time() с параметър NULL; функцията е дефинирана в time.h. Ако изпълнената програма е била прекъсната от подаден сигнал, това се приема за завършване с код за грешка
    //129.

---
+++ C/2019-SE-01-p.c
@@ -0,0 +1,113 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <err.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
+#include <stdio.h>
+
+int execChild(char * args[]){
+	int pid = fork();
+	if (pid == -1)
+	{
+		err(1,"could not fork");
+	}
+	if (pid == 0)
+	{
+		if (execvp(args[0],args) == -1)
+		{
+			err(2, "could not exec");
+		}
+	}
+	int status;
+	wait(&status);
+	if(WIFEXITED(status)) {
+		return WEXITSTATUS(status);
+	}else if(WIFSIGNALED(status))
+	{
+		return 128;
+	}
+	return 1;
+}
+
+
+void flog(time_t start, time_t end, int status)
+{
+	int fd = open("log.txt", O_RDWR|O_APPEND|O_CREAT, 0644);
+	if (fd == -1)
+	{
+		err(4,"could not open log.txt");
+	}
+	char logline[30];
+
+	ssize_t len =0 ;
+
+	sprintf(logline, "%ld, %ld %d\n", start,end,status);
+	len= strlen(logline);
+
+	if(write(fd,logline,len) != len)
+	{
+		err(5,"could not write into log.txt");
+	}
+
+	close(fd);
+
+}
+
+
+bool execute(int lim, char *args[])
+{
+	time_t start = time(NULL);
+	int status = execChild(args);
+	time_t end = time(NULL);
+
+	flog(start,end,status);
+
+	return (((end-start)<lim) && status!=0);
+}
+
+
+int main(int argc, char* argv[])
+{
+	if ( argc < 3)
+	{
+		errx(3,"Invalid number of arguments");
+	}
+
+	//char c = argv[1][0];
+	//if(c > '9' || c < '0' || argv[1][1] != '\0')
+	//	errx(99, "Invalid digit argument");
+
+	//int sec = (argv[1][0] - '0');
+
+	int c = atoi(argv[1]);
+
+	if(c > 9 || c < 0 )
+	{	errx(99, "Invalid digit argument"); }
+
+	int sec = c;
+
+	char *args[12];
+
+	int i = 2, cnt = 0;
+	for(;i<argc;i++)
+	{
+		args[cnt] = argv[i];
+		cnt++;
+	}
+	args[cnt] = NULL;
+
+	int condcnt = 0;
+	while( condcnt < 2)
+	{
+		if(execute(sec,args) != 0)
+			condcnt++;
+		else
+			condcnt = 0;
+	}
+
+	exit(0);
+}
